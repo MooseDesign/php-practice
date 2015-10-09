0d070e0abb215e79a0aaafe5eff7a322bd4bd9f3
MooseDesign <richard@moosedesign.com>
2015-10-08 19:59:46 -0600

    HTML to PHP Conversions

---
+++ footer.php
@@ -0,0 +1,10 @@
+	<footer>
+	
+		<?php include 'nav.php'; ?>
+	
+	  <!-- Site Copyright -->
+	    <p>Copyright 2015 - <?php echo $userName; ?></p>
+	</footer>
+
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,30 @@
+<?php 
+	$pageID = strtolower($pageTitle); 
+	$userName = 'Moose';
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Adding the Page Title from the index, about, or contact pages -->
+    <title><?php echo $pageTitle; ?> | <?php echo $userName; ?></title>
+
+    <!-- Tell the browser to render the view width correctly -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Link our stylesheet -->
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body id="<?php echo $pageID; ?>">
+	  
+	  <header>
+	  <div class="row clearfix">
+	
+	    <!-- Our Logo -->
+	    <a href="index.php"><img class="logo" src="img/logo.png"/></a>
+		    
+	    <?php include 'nav.php'; ?>
+	
+	  </div>
+	</header>
